566dd668a8b5d436af26830dbfe97ead9fa71630
Ghorphindhel <pj.lacroix@live.fr>
2026-03-30 15:40:14 +0200

    Add files via upload

---
+++ Logs_Teams.ps1
@@ -0,0 +1,3 @@
+﻿Test-Path "$env:LOCALAPPDATA\Packages\MSTeams_8wekyb3d8bbwe"
+Get-ChildItem "$env:LOCALAPPDATA\Packages\MSTeams_8wekyb3d8bbwe\LocalCache\Microsoft\MSTeams" -Force
+Get-ChildItem "$env:LOCALAPPDATA\Packages\MSTeams_8wekyb3d8bbwe\LocalCache\Microsoft\MSTeams\Logs" -File -ErrorAction SilentlyContinue
